523821d7beeb9fc44ef19c6bd587c07c4b6d49c6
Dieter Plaetinck <dieter@plaetinck.be>
2010-12-01 14:32:35 +0100

    add more info messages so you know what's going on

--- git-remote-in-sync.sh
+++ git-remote-in-sync.sh
@@ -20,24 +20,37 @@ if [ -z "`git branch`" ]
 then
 	if [ $(cd $(git root) && ls -Al | egrep -v "^(.git|total)" | wc -l) -gt 0 ]
 	then
-		echo "This repo doesn't contain any branch, but contains a bunch of files!" >&2
+		echo "WARN: This repo doesn't contain any branch, but contains a bunch of files!" >&2
 		ls -Alh $(git root)
 		ret=1
 	else
-		echo "This repo doesn't contain any branch, and is empty"
+		echo "INFO: This repo doesn't contain any branch, and is empty"
 		# note that stashing doesn't work without a branch, so the above check is sufficient
 		exit 0
 	fi
+else
+	echo -n "INFO: Valid git repo with branches: "
+	git branch | tr '\n' ' '
+	echo
 fi
 
 if [ -z "$list" ]
 then
-	echo "At least one branch, but no remotes found!  The content here might be unique!" >&2
+	echo "WARN: At least one branch, but no remotes found!  The content here might be unique!" >&2
 	ret=1
+else
+	if [ -n "$@" ]
+	then
+		echo "INFO: working with remote(s): $@"
+		echo "INFO: fyi, all remotes: "`git remote`
+	else
+		echo "INFO: working with all remotes: $list"
+	fi
 fi
 
 for remote in $list;
 do
+	echo "INFO: Checking remote $remote.."
 	# Check commits and branches
 	# An approach where you `git push --all -n $remote 2>&1` and check whether the result is
 	# 'Everything up-to-date' can (and often will) yield
@@ -46,7 +59,7 @@ do
 	# and inspect it's branches.  This is a bit inefficient network-wise, but couldn't see a better solution
 	if ! git fetch $remote
 	then
-		echo "could not git fetch $remote" >&2
+		echo "  WARN: could not git fetch $remote" >&2
 		ret=1
 	fi
 	IFS_BACKUP=$IFS
@@ -57,13 +70,17 @@ do
 		branch_local=${branch// /}; # git branch prefixes branches with spaces. and spaces in branchnames are illegal.
 		if ! git branch -r --contains $branch_local 2>/dev/null | grep -q "^  $remote/"
 		then
-			echo "Branch $branch_local is not contained within remote $remote!" >&2
+			echo "  WARN: Branch $branch_local is not contained within remote $remote!" >&2
 			ret=1
+		else
+			echo "  INFO: Branch $branch_local is contained within remote $remote"
 		fi
 		if ! git branch -r | grep -q "^  $remote/$branch_local"
 		then
-			echo "Branch $branch_local exists, but not $remote/$branch_local" >&2
+			echo "  WARN: Branch $branch_local exists, but not $remote/$branch_local" >&2
 			ret=1
+		else
+			echo "  INFO: Branch $branch_local exists also as $remote/$branch_local"
 		fi
 	done
 	IFS=$IFS_BACKUP
@@ -72,8 +89,10 @@ do
 	out=`git push --tags -n $remote 2>&1`
 	if [ "$out" != 'Everything up-to-date' ];
 	then
-		echo -e "***** Dirty tags: *****\n$out" >&2
+		echo -e "  WARN: Some tags are not in $remote!\n$out" >&2
 		ret=1
+	else
+		echo "  INFO: All tags ("`git tag`") exist at $remote as well"
 	fi
 done
 
@@ -83,24 +102,33 @@ cur_branch=${cur_branch// /}
 exp="# On branch $cur_branch
 nothing to commit (working directory clean)"
 out=`git status`
+wc_ok=1
 if [ "$out" != "$exp" ]
 then
 	# usually i'd use bash regex, but this case is multiple-lines so bash regex is no go
 	out=$(echo "$out" | egrep -v "^(# On branch $cur_branch|# Your branch is behind .* commits, and can be fast-forwarded.|#|nothing to commit \(working directory clean\))$")
 	if [ -n "$out" ]
 	then
-		echo "***** Dirty WC or index *****" >&2
+		wc_ok=0
+		echo "WARN: Dirty WC or index" >&2
 		git status
 		ret=1
 	fi
+fi
+if [ $wc_ok -eq 1 ]
+then
+	echo "INFO: Working copy/index are clean"
+	git status
 fi
 
 # Check stash
 if [ `git stash list | wc -l` -gt 0 ];
 then
-	echo "***** Dirty stash: *****" >&2
-	GIT_PAGER= git stash list
+	echo "WARN: Dirty stash:" >&2
+	GIT_PAGER= git stash list >&2
 	ret=1
+else
+	echo "INFO: Stash clean"
+	GIT_PAGER= git stash list
 fi
-
 exit $ret
