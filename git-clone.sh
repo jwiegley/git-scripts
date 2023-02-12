9a2edb0ee69ad94686d414f3e46fbf09c91c9b76
Edwin Kofler <edwin@kofler.dev>
2023-02-12 11:32:57 -0800

    chore: More Bash improvements

--- git-clone.sh
+++ git-clone.sh
@@ -3,28 +3,21 @@
 # this script clones a repository, including all its remote branches
 # Author: julianromera
 
-GIT=$(command -v git)
-
-if [ "$1" = "" -o "$2" = "" ];then
+if [[ -z "$1" || -z "$2" ]];then
   echo "use: $0 <git_repository_to_clone> <directory>"
   exit 1
 fi
 
-if [ "$GIT" = "" ];then
-  echo "No git command found. install it"
-fi
 
 function clone {
 
-  $GIT clone -q "$1" "$2"
-  res=$?
-
+  git clone -q "$1" "$2"
   cd "$2"
 
-  $GIT pull --all
+  git pull --all
 
-  for remote in $($GIT branch -r | grep -v \>); do
-     $GIT branch --track "${remote#origin/}" "$remote";
+  for remote in $(git branch -r | grep -v \>); do
+     git branch --track "${remote#origin/}" "$remote";
   done
 }
 
